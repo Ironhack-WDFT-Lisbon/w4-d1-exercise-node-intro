cb67c9ea1f233db3dd7aedf6f1ba62f9ad02665b
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-29 11:19:16 +0100

    update gitignore

---
+++ index.js
@@ -0,0 +1,13 @@
+const moment = require("moment");
+const superheroes = require("superheroes");
+const cows = require("cows");
+const cool = require("cool-ascii-faces");
+
+// const date = moment().format("MMMM Do YYYY, h:mm:ss a");
+// console.log(date);
+
+// console.log(superheroes.random());
+
+// console.log(cows());
+
+console.log(cool());
